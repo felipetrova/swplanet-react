afc468692bb66d79d057519f45bb16759c77b7e7
Felipe Trova <felipee.trova@gmail.com>
2020-03-22 12:32:24 -0300

    Add base project

---
+++ styles/Icons.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Icon = styled.i`
+  font-family: 'Font Awesome 5 Free' !important;
+  speak: none;
+  font-style: normal;
+  font-weight: normal;
+  font-variant: normal;
+  text-transform: none;
+  line-height: 1;
+
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+`;
